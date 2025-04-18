3a484bb4092df35309fbbc8d46e2363adb75eead
Zachary Sexton <47196674+zasexton@users.noreply.github.com>
2025-04-18 11:43:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 from cmake_setuptools import CMakeExtension, CMakeBuildExt
 
 setup(
-    ext_modules=[CMakeExtension("pysvzerod", install=True)],
+    ext_modules=[CMakeExtension("pysvzerod")],
     cmdclass={"build_ext": CMakeBuildExt},
 )
